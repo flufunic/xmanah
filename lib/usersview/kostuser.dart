b68309cc4b94510e340157307c6f402bd0abaa5a
Mut127 <muthiakhanza034@gmail.com>
2024-11-29 15:07:47 +0700

    datail view for kost page (inkost)

--- lib/usersview/kostuser.dart
+++ lib/usersview/kostuser.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:xmanah/controller/kost.dart';
+import 'package:xmanah/usersview/inkost.dart';
 
 class KostUser extends StatelessWidget {
   @override
@@ -36,6 +37,9 @@ class KostUser extends StatelessWidget {
                   name: kost['nama'] ?? 'Nama Kost',
                   address: kost['alamat'] ?? 'Alamat Kost',
                   review: kost['ulasan'] ?? 'Ulasan',
+                  kontak: kost['kontak'] ?? 'Kontak',
+                  fasilitas: kost['fasilitas'] ?? 'Fasilitas',
+                  harga: kost['harga'] ?? 'Harga',
                 );
               },
             );
@@ -51,6 +55,9 @@ class KostCard extends StatelessWidget {
   final String name;
   final String address;
   final String review;
+  final String kontak;
+  final int harga;
+  final String fasilitas;
 
   const KostCard({
     Key? key,
@@ -58,86 +65,107 @@ class KostCard extends StatelessWidget {
     required this.name,
     required this.address,
     required this.review,
+    required this.kontak,
+    required this.harga,
+    required this.fasilitas,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      margin: EdgeInsets.all(16.0),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(8.0),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black12,
-            blurRadius: 6.0,
-            offset: Offset(0, 3),
-          ),
-        ],
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.vertical(top: Radius.circular(8.0)),
-            child: imageUrl.isNotEmpty
-                ? Image.network(
-                    imageUrl,
-                    height: 180,
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                  )
-                : Container(
-                    height: 180,
-                    width: double.infinity,
-                    color: Colors.grey[300],
-                    child: Icon(Icons.image, color: Colors.white, size: 40),
-                  ),
-          ),
-          // Nama, Alamat, dan Ulasan Kost
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  name,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(height: 4.0),
-                Text(
-                  address,
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    color: Colors.grey[600],
-                  ),
-                ),
-                SizedBox(height: 8.0),
-                Row(
-                  children: [
-                    Icon(
-                      Icons.star,
-                      color: Colors.yellow[700],
-                      size: 20.0,
-                    ),
-                    SizedBox(width: 4.0),
-                    Text(
-                      review,
-                      style: TextStyle(
-                        fontSize: 16.0,
-                        color: Colors.grey[800],
-                      ),
-                    ),
-                  ],
-                ),
-              ],
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => KostDetail(
+              imageUrl: imageUrl,
+              name: name,
+              address: address,
+              review: review,
+              kontak: kontak,
+              harga: harga,
+              fasilitas: fasilitas,
             ),
           ),
-        ],
+        );
+      },
+      child: Container(
+        margin: EdgeInsets.all(16.0),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(8.0),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black12,
+              blurRadius: 6.0,
+              offset: Offset(0, 3),
+            ),
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Gambar Kost
+            ClipRRect(
+              borderRadius: BorderRadius.vertical(top: Radius.circular(8.0)),
+              child: imageUrl.isNotEmpty
+                  ? Image.network(
+                      imageUrl,
+                      height: 180,
+                      width: double.infinity,
+                      fit: BoxFit.cover,
+                    )
+                  : Container(
+                      height: 180,
+                      width: double.infinity,
+                      color: Colors.grey[300],
+                      child: Icon(Icons.image, color: Colors.white, size: 40),
+                    ),
+            ),
+            // Nama, Alamat, dan Ulasan Kost
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    name,
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 4.0),
+                  Text(
+                    address,
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      color: Colors.grey[600],
+                    ),
+                  ),
+                  SizedBox(height: 8.0),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.star,
+                        color: Colors.yellow[700],
+                        size: 20.0,
+                      ),
+                      SizedBox(width: 4.0),
+                      Text(
+                        review,
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          color: Colors.grey[800],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
